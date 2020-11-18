b1e0243b8ade6828f11dcc8f5db5567f8ca18670
Sidhant Bhavnani <sidhant.gla@gmail.com>
2020-11-18 14:02:49 +0530

    Docker works!

---
+++ BusinessSimulator/wait-for-mysql.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+while ! nc -z db 3306 ; do
+    echo "Waiting for the MySQL Server"
+    sleep 3
+done
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 db_populate
+python3 manage.py runserver 0.0.0.0:8000

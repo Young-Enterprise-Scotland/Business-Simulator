955df0d1271aefd55d86aa981aa617c7cf8635a6
Nathan Welsh <2381793w@student.gla.ac.uk>
2020-11-25 16:19:56 +0000

    School Account form development

--- BusinessSimulator/simulatorApp/urls.py
+++ BusinessSimulator/simulatorApp/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('login', views.Login.as_view(), name='login'),
     path('logout', views.Logout.as_view(), name='logout'),
-    path('yesProfile',views.YesProfile.as_view(),name='viewYesAccount')
+    path('yesProfile',views.YesProfile.as_view(),name='viewYesAccount'),
+    path('schoolProfile',views.YesProfile.as_view(),name='schoolAccountView')
 ]

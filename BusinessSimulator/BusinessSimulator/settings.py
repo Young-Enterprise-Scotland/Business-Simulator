c7f95c085c08fb8e7e4a35444b8fefa0f68a7c36
Sidhant Bhavnani <sidhant.gla@gmail.com>
2020-11-25 06:56:35 +0530

    install processor Sass, remove alpine, install vanilla python, remove unnecessary repeated files

--- BusinessSimulator/BusinessSimulator/settings.py
+++ BusinessSimulator/BusinessSimulator/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sass_processor',
     'simulatorApp'
 ]
 
@@ -159,4 +160,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Incase of shit hitting the fan due to this malfunctioning, please refer to https://github.com/jrief/django-sass-processor
+SASS_PROCESSOR_ROOT = os.path.join(STATIC_ROOT, 'assets')
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
+
+SASS_OUTPUT_STYLE = 'compact'

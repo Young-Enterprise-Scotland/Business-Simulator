79f7aea1be8c0ce1f4d1a736737c86bc31013c59
Nathan Welsh <2381793w@student.gla.ac.uk>
2020-12-16 17:18:41 +0000

    Added Features: - YES can access Teams - Policy Model Created - Policy objects auto crated whenever a simulator model is created - PolicyStrategy objects auto created for Team objects - Updated Tests to to reflect YES name column being dropped

---
+++ BusinessSimulator/simulatorApp/globals.py
@@ -0,0 +1,17 @@
+#
+# File to define global functions, variables for general use
+#
+
+POLICIES = [
+        "Market Research",
+        "Product Development",
+        "Quality of Raw Materials",
+        "Appearance of Packaging",
+        "Physical Position in Market",
+        "Size of Unit",
+        "Internal Appearance",
+        "Targeted Online Adverts",
+        "Physical Print (flyers/posters)",
+        "Newspaper Advert"
+    ]
+
